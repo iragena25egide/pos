708cb68468e220fadc2ed45d2fc661d707b5b78e
iragena25egide <iragenaegide205@gmail.com>
2026-06-14 18:39:30 +0200

    some error handling

--- api/backends.py
+++ api/backends.py
@@ -4,19 +4,23 @@ from django.db.models import Q
 
 User = get_user_model()
 
+
 class EmailAuthBackend(ModelBackend):
     """
     Custom authentication backend to allow users to log in using either their username or email address.
     """
+
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            # Check if the user exists by either username or email
-            user = User.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+
+            user = User.objects.get(
+                Q(username__iexact=username) | Q(email__iexact=username))
         except User.DoesNotExist:
             return None
         except User.MultipleObjectsReturned:
             # If multiple users have the same email, get the first one
-            user = User.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).first()
+            user = User.objects.filter(
+                Q(username__iexact=username) | Q(email__iexact=username)).first()
 
         if user and user.check_password(password) and self.user_can_authenticate(user):
             return user
